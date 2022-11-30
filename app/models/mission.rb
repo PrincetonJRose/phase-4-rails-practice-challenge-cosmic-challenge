52c4dd660a7f35ee681266205ab8e90d633c3086
Princeton J. Rose <princetonjrose@gmail.com>
2022-11-30 09:50:46 -0500

    fixed last of validations

--- app/models/mission.rb
+++ app/models/mission.rb
@@ -3,13 +3,13 @@ class Mission < ApplicationRecord
   belongs_to :planet
 
   validates :name, :planet, :scientist, presence: true
-  validates :scientist, uniqueness: { scope: :name }
+  # validates :scientist, uniqueness: { scope: :name }
 
-  # validate :join_mission_once
+  validate :join_mission_once, on: :create
 
-  # def join_mission_once
-  #   if Mission.find_by( name: self.name, planet: self.planet, scientist: self.scientist )
-  #     errors.add(:scientist_id, "Can't join the same mission more than once.")
-  #   end
-  # end
+  def join_mission_once
+    if Mission.find_by( name: self.name, planet: self.planet, scientist: self.scientist )
+      errors.add(:scientist, "can't join the same mission more than once. Sorry space cowboy.")
+    end
+  end
 end
--- app/models/scientist.rb
+++ app/models/scientist.rb
@@ -3,7 +3,7 @@ class Scientist < ApplicationRecord
     has_many :missions
     has_many :planets, through: :missions
 
-    validates :name, presence: true, uniqueness: true, on: :create, on: :update
+    validates :name, presence: true, uniqueness: true, on: [:create, :update]
     validates :field_of_study, presence: true
 
 end
